632f509f1077dfe4b49eb1f5d077df6a32ad4b5a
Andrei <n3rd4i@gmail.com>
2019-09-02 18:42:26 +0200

    update to version 2019.35.5.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
 $unzipLocation = "$(Join-Path $toolsDir TeslaCamViewer)"
-$url        = 'https://github.com/NateMccomb/TeslaCamViewer/releases/download/2019.32.0.0/Standalone-TeslaCamViewer-2019.32.0.0.zip'
+$url        = 'https://github.com/NateMccomb/TeslaCamViewer/releases/download/2019.35.5.0/Standalone-TeslaCamViewer-2019.35.5.0.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Join-Path $toolsDir TeslaCamViewer)"
   url           = $url
   softwareName  = 'teslacamviewer*'
-  checksum      = '197075F679EE9000DE9127D75E5F1D90D243240464E6E4E443A38453B0E3BC2C'
+  checksum      = 'A72ED8CE6C897B5A0A7F9832AB8237EC582D29BEF27A61609D701832C387119C'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
