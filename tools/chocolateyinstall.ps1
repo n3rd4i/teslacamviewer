36086f4f53f4b72db6cc0cea315902aeff43bb24
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-12-18 13:03:27 +0100

    upgraded to version II -> 2019.46.6.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,18 +2,16 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$unzipLocation = "$(Join-Path $toolsDir TeslaCamViewer)"
-$url        = 'https://github.com/NateMccomb/TeslaCamViewer/releases/download/2019.35.5.0/Standalone-TeslaCamViewer-2019.35.5.0.zip'
+$url        = 'https://github.com/NateMccomb/TeslaCamViewerII/releases/download/2019.46.6.0/Standalone-TeslaCamViewer-2019.46.6.0.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = "$(Join-Path $toolsDir TeslaCamViewer)"
+  unzipLocation = "$installLocation"
   url           = $url
-  softwareName  = 'teslacamviewer*'
-  checksum      = 'A72ED8CE6C897B5A0A7F9832AB8237EC582D29BEF27A61609D701832C387119C'
+  checksum      = '9AFFEE3CE7863FEAEAF599E83316CE2FA1E8CEB246FC1B3152B48F051FAEDBBF'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
 Install-ChocolateyShortcut -ShortcutFilePath "$shortcutPath" `
-  -TargetPath "`"$unzipLocation\$AppName.exe`"" `
-  -WorkingDirectory "$unzipLocation"
+  -TargetPath "$(Join-Path $installLocation `"$AppName.exe`")" `
+  -WorkingDirectory "$installLocation"
--- tools/commonEnv.ps1
+++ tools/commonEnv.ps1
@@ -1,2 +1,8 @@
-$AppName = 'TeslaCam Viewer'
-$shortcutPath = [IO.Path]::Combine($ENV:UserProfile, 'Desktop', "$AppName.lnk")
+## Common between Install and Uninstall
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$AppName = 'TeslaCam Viewer II'
+$installLocation = "$(Join-Path $toolsDir $AppName)"
+
+$startMenuDir = [IO.Path]::Combine($ENV:ProgramData, 'Microsoft\Windows\Start Menu\Programs', $AppName)
+$shortcutPath = "$(Join-Path $startMenuDir `"$AppName.lnk`")"
