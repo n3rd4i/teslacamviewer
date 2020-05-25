dedcbc7e5264578fbeab2d484e312e5cab58625f
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-05-25 23:44:33 +0200

    updated to version 2020.18.0.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url        = 'https://github.com/NateMccomb/TeslaCamViewerII/releases/download/2020.11.0.0/Standalone-TeslaCamViewer-2020.11.0.0.zip'
+$url        = 'https://github.com/NateMccomb/TeslaCamViewerII/releases/download/2020.18.0.0/Standalone-TeslaCamViewer-2020.18.0.0.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$installLocation"
   url           = $url
-  checksum      = '2471D41377BF4EF552C69FEF8C2710C0590B5691261F6D5391873E871F1B7193'
+  checksum      = 'BECE3851494555E3D20E095F7C5A219C3EA6918707DD94F697E6FEB1ADE62CB3'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
